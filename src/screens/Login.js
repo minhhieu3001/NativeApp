ccfa4bd9810155e57b3ed7c093f082be2d6b6b6d
NTQ-SOLUTION\hieu.bui <NTQ-SOLUTION\hieu.bui>
2022-09-05 17:44:12 +0700

    login_screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { TextInput, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import CustomInput from "../components/CustomInput";
 import CustomButton from '../components/CustomButton';
 
@@ -7,14 +7,35 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const onLoginPressed = () => {
+        console.warn("Log in");
+    }
+
+    const onResetPasswordPressed = () => {
+        console.warn("Reset password");
+    }
+
     return (
         <View>
-            <CustomInput placeholder="Email" value={email} setValue={setEmail} iconName="email"/>
-            <CustomInput placeholder="Password" value={password} setValue={setPassword} password={true} iconName="password"/>
+            <CustomInput placeholder="Email" value={email} setValue={setEmail} iconName="email.png" image="email"/>
+            <CustomInput placeholder="Password" value={password} setValue={setPassword} password={true} image="pass"/>
 
+            <CustomButton text="Log in" onPress={onLoginPressed}/>
+            <CustomButton text="Reset password?" onPress={onResetPasswordPressed} type="TERTIARY" />
+
+            <Text style={styles.text}>--- or ---</Text>
+
+            <CustomButton />
             <CustomButton />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    text: {
+        textAlign : 'center',
+        marginBottom : 10
+    }
+})
 
 export default Login
